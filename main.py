b4b4b1cef593a83092ce344d3a03f0c962abad92
Haydenchung03 <Haydenchung21@hotmail.com>
2022-04-20 01:48:08 -0400

    Created a menu and detect bugs

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from pytz import timezone
 def main():
     
     while(True):
-        now = datetime.now()
+        #now = datetime.now()
         tz = timezone('EST')
         current_time = datetime.now(tz)
         comma = ','
@@ -21,29 +21,41 @@ def main():
         getName = getMonth1.numToMonth()
         
         print("Main Menu")
+        print("Type FindPrice to get price of stock")
+        print("Type Quit to exit")
+        user_option = input("What do you want to do: ")
+        print()
         print(f'Current Time = {getName} {current_time.day}{comma}{current_time.year}')
         print(f"Current Time = {current_time.hour + 1}:{current_time.minute}:{current_time.second}")
-        #user_option = input(" ")
-        user_input = input('Enter in a ticker: ')
-
-        user_stock = stock(user_input)
         
-        get_stock = user_stock.get_ticker()
-        
-        url = f'https://finance.yahoo.com/quote/{get_stock}/'
+        if user_option == 'Quit':
+            break
+        elif user_option == 'FindPrice':
+            print()
+            user_input = input('Enter in a ticker: ')
+            try:
+                user_stock = stock(user_input)
+                
+                get_stock = user_stock.get_ticker()
+                
+                url = f'https://finance.yahoo.com/quote/{get_stock}/'
 
-        response = requests.get(url)
+                response = requests.get(url)
 
-        soup = BeautifulSoup(response.text, "lxml")
+                soup = BeautifulSoup(response.text, "lxml")
 
-        if current_time.hour >= 9.5 and current_time.hour < 16:
-            current_price = soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px) W(100%)'})[0].find('fin-streamer').text
+                if current_time.hour >= 9.5 and current_time.hour < 16:
+                    current_price = soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px) W(100%)'})[0].find('fin-streamer').text
 
-            print(f'This is the price of the current stock: {current_price}')
+                    print(f'This is the price of the current stock: {current_price}')
+                else:
+                    extended_hours = soup.find('fin-streamer', {'class': "C($primaryColor) Fz(24px) Fw(b)"}).text
+                    
+                    print(f'This is the price of the stock in extended trading hours: {extended_hours}')
+            except:
+                print('Oops. Either the ticker was not inputted correctly or internet was cut off.')
         else:
-            extended_hours = soup.find('fin-streamer', {'class': "C($primaryColor) Fz(24px) Fw(b)"}).text
-            
-            print(f'This is the price of the stock in extended trading hours: {extended_hours}');
+            print('Not a valid option')
 
 main()
 
